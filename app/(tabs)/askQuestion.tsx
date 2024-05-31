edbce1eeaf7246a434229b0bf1770b3e528a7067
Hadi <hadielali@web.de>
2024-05-31 21:22:42 +0200

    i

--- app/(tabs)/askQuestion.tsx
+++ app/(tabs)/askQuestion.tsx
@@ -1,41 +1,31 @@
-//dasboard-neu:
+import React, { useState, useRef } from "react";
 import {
   KeyboardAvoidingView,
   Keyboard,
   ScrollView,
   StyleSheet,
   TextInput,
+  View,
+  Text,
+  TouchableWithoutFeedback,
+  Pressable,
+  Modal,
+  Alert,
+  Platform,
 } from "react-native";
-import { View, Text } from "components/Themed";
+import { Stack, Link } from "expo-router";
 import Colors from "constants/Colors";
-import { TouchableWithoutFeedback } from "react-native";
-import { Pressable, Modal } from "react-native";
-import { Stack } from "expo-router";
 import { useColorScheme } from "react-native";
 import { coustomTheme } from "components/coustomTheme";
-import { useState } from "react";
 import Checkbox from "expo-checkbox";
 import { useSendQuestion } from "components/useSendQuestion";
 import Toast from "react-native-toast-message";
-import { router } from "expo-router";
-import { Alert } from "react-native";
-import { Platform } from "react-native";
-import { useRef } from "react";
-import { Link } from "expo-router";
 import { Picker } from "@react-native-picker/picker";
 
-interface Email {
-  name: string;
-  age: number;
-  email: string;
-  marja: string;
-  gender: string;
-  question: string;
-}
 const genderOptions = [
   { label: "-- Wähle bitte dein Geschlecht aus --", value: "default" },
   { label: "Männlich", value: "Männlich" },
-  { label: "Weiblich ", value: "Weiblich" },
+  { label: "Weiblich", value: "Weiblich" },
 ];
 
 const marjaOptions = [
@@ -49,14 +39,14 @@ export default function askQuestion() {
   const colorScheme = useColorScheme();
   const themeStyles = coustomTheme(colorScheme);
 
-  const [name, setName] = useState<string>("");
-  const [age, setAge] = useState<string>("");
-  const [email, setEmail] = useState<string>("");
-  const [validateEmail, setValidateEmail] = useState<string>("");
-  const [marja, setMarja] = useState<string>(marjaOptions[0].label);
-  const [gender, setgender] = useState<string>(genderOptions[0].label);
-  const [question, setQuestion] = useState<string>("");
-  const [acceptRules, setAcceptRules] = useState<boolean>(false);
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [email, setEmail] = useState("");
+  const [validateEmail, setValidateEmail] = useState("");
+  const [marja, setMarja] = useState(marjaOptions[0].value);
+  const [gender, setGender] = useState(genderOptions[0].value);
+  const [question, setQuestion] = useState("");
+  const [acceptRules, setAcceptRules] = useState(false);
   const [isPickerVisibleMarja, setIsPickerVisibleMarja] = useState(false);
   const [isPickerVisibleGender, setIsPickerVisibleGender] = useState(false);
   const { sendEmail } = useSendQuestion();
@@ -64,13 +54,7 @@ export default function askQuestion() {
   const scrollViewRef = useRef(null);
 
   const validateForm = () => {
-    if (
-      !age ||
-      !email ||
-      !validateEmail ||
-      !question ||
-      question.trim() === ""
-    ) {
+    if (!age || !email || !validateEmail || !question || question.trim() === "") {
       Alert.alert("Fehler", "Bitte fülle alle Pflichtfelder aus!");
       return false;
     }
@@ -92,11 +76,11 @@ export default function askQuestion() {
       return false;
     }
 
-    if (gender == genderOptions[0].label) {
+    if (gender === genderOptions[0].value) {
       Alert.alert("Fehler", "Bitte wähle dein Geschlecht aus!");
       return false;
     }
-    if (marja == marjaOptions[0].label) {
+    if (marja === marjaOptions[0].value) {
       Alert.alert("Fehler", "Bitte wähle einen Marja aus!");
       return false;
     }
@@ -114,14 +98,7 @@ export default function askQuestion() {
 
   const send = async () => {
     if (validateForm()) {
-      const success = await sendEmail(
-        name,
-        age,
-        email,
-        marja,
-        gender,
-        question
-      );
+      const success = await sendEmail(name, age, email, marja, gender, question);
       if (success) {
         Toast.show({
           type: "success",
@@ -145,15 +122,14 @@ export default function askQuestion() {
       behavior={Platform.OS === "ios" ? "padding" : "height"}
       keyboardVerticalOffset={Platform.OS === "ios" ? 64 : 0}
     >
-      <TouchableWithoutFeedback accessible={false} onPress={Keyboard.dismiss}>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
         <View style={styles.container}>
-          {/* Submit button */}
           <Stack.Screen
             options={{
               headerTitle: "Eine Frage stellen",
               headerRight: () => (
                 <View style={styles.headerButton}>
-                  <Pressable onPress={() => send()}>
+                  <Pressable onPress={send}>
                     <Text style={styles.submitButtonText}>Senden</Text>
                   </Pressable>
                 </View>
@@ -163,148 +139,89 @@ export default function askQuestion() {
           <ScrollView
             contentContainerStyle={styles.contactContainer}
             ref={scrollViewRef}
-            // Makes Question input jump up when keyboard visible
-            onContentSizeChange={() =>
-              scrollViewRef.current.scrollToEnd({ animated: true })
-            }
+            onContentSizeChange={() => scrollViewRef.current.scrollToEnd({ animated: true })}
           >
             <TextInput
-              style={[
-                styles.input,
-                styles.inputName,
-                themeStyles.inverseTextInput,
-              ]}
+              style={[styles.input, themeStyles.inverseTextInput]}
               onChangeText={setName}
               value={name}
               placeholder='Name (optional)'
               keyboardType='default'
             />
             <TextInput
-              style={[
-                styles.input,
-                styles.inputAge,
-                themeStyles.inverseTextInput,
-              ]}
+              style={[styles.input, themeStyles.inverseTextInput]}
               onChangeText={setAge}
               value={age}
               placeholder='Alter (Pflicht)'
               keyboardType='numeric'
             />
             <TextInput
-              style={[
-                styles.input,
-                styles.inputEmail,
-                themeStyles.inverseTextInput,
-              ]}
+              style={[styles.input, themeStyles.inverseTextInput]}
               onChangeText={setEmail}
               value={email}
               placeholder='E-Mail (Pflicht)'
               keyboardType='email-address'
             />
             <TextInput
-              style={[
-                styles.input,
-                styles.inputEmail,
-                themeStyles.inverseTextInput,
-              ]}
+              style={[styles.input, themeStyles.inverseTextInput]}
               onChangeText={setValidateEmail}
               value={validateEmail}
               placeholder='E-Mail wiederholen (Pflicht)'
               keyboardType='email-address'
             />
-            {/* Gender */}
-            <View style={[styles.ContainerModal, themeStyles.inverseQuestionBackground]}>
-              <TextInput
-                onPress={() => setIsPickerVisibleGender(true)}
-                style={[styles.modalTextInput, themeStyles.inverseQuestionText]}
-                value={gender}
-                editable={false}
-              />
-            </View>
+
+            {/* Gender Picker */}
+            <TouchableWithoutFeedback onPress={() => setIsPickerVisibleGender(true)}>
+              <View style={[styles.pickerTrigger, themeStyles.inverseTextInput]}>
+                <Text style={styles.pickerText}>{genderOptions.find(option => option.value === gender)?.label}</Text>
+              </View>
+            </TouchableWithoutFeedback>
             <Modal
               visible={isPickerVisibleGender}
               transparent={true}
               animationType='slide'
               onRequestClose={() => setIsPickerVisibleGender(false)}
             >
-              <View
-                style={[
-                  styles.modalContainer,
-                  themeStyles.modalQuestionBlurredBackground,
-                ]}
-              >
-                <View
-                  style={[styles.pickerContainer, themeStyles.modalQuestion]}
-                >
+              <View style={styles.modalContainer}>
+                <View style={styles.pickerContainer}>
                   <Picker
                     selectedValue={gender}
                     onValueChange={(itemValue) => {
-                      setgender(itemValue);
-
-                      // Dismiss Picker
+                      setGender(itemValue);
                       setIsPickerVisibleGender(false);
                     }}
                   >
                     {genderOptions.map((option) => (
-                      <Picker.Item
-                        key={option.label}
-                        label={option.label}
-                        value={option.label}
-                        color={
-                          colorScheme == "light"
-                            ? Colors.light.modalQuestionText
-                            : Colors.dark.modalQuestionText
-                        }
-                      />
+                      <Picker.Item key={option.value} label={option.label} value={option.value} />
                     ))}
                   </Picker>
                 </View>
               </View>
             </Modal>
-            {/* Marja */}
-            <View style={[styles.ContainerModal, themeStyles.inverseQuestionBackground]}>
-              <TextInput
-                onPress={() => setIsPickerVisibleMarja(true)}
-                style={[styles.modalTextInput, themeStyles.inverseQuestionText]}
-                value={marja}
-                editable={false}
-              />
-            </View>
+
+            {/* Marja Picker */}
+            <TouchableWithoutFeedback onPress={() => setIsPickerVisibleMarja(true)}>
+              <View style={[styles.pickerTrigger, themeStyles.inverseTextInput]}>
+                <Text style={styles.pickerText}>{marjaOptions.find(option => option.value === marja)?.label}</Text>
+              </View>
+            </TouchableWithoutFeedback>
             <Modal
               visible={isPickerVisibleMarja}
               transparent={true}
               animationType='slide'
               onRequestClose={() => setIsPickerVisibleMarja(false)}
             >
-              <View
-                style={[
-                  styles.modalContainer,
-                  themeStyles.modalQuestionBlurredBackground,
-                ]}
-              >
-                <View
-                  style={[styles.pickerContainer, themeStyles.modalQuestion]}
-                >
+              <View style={styles.modalContainer}>
+                <View style={styles.pickerContainer}>
                   <Picker
                     selectedValue={marja}
                     onValueChange={(itemValue) => {
                       setMarja(itemValue);
-
-                      // Dismiss Picker
                       setIsPickerVisibleMarja(false);
                     }}
                   >
                     {marjaOptions.map((option) => (
-                      <Picker.Item
-                        key={option.label}
-                        label={option.label}
-                        value={option.label}
-                        color={
-                          colorScheme == "light"
-                            ? Colors.light.modalQuestionText
-                            : Colors.dark.modalQuestionText
-                        }
-                      />
+                      <Picker.Item key={option.value} label={option.label} value={option.value} />
                     ))}
                   </Picker>
                 </View>
@@ -315,26 +232,18 @@ export default function askQuestion() {
               <Checkbox
                 style={styles.rulesCheckbox}
                 value={acceptRules}
-                onValueChange={() => setAcceptRules(!acceptRules)}
+                onValueChange={setAcceptRules}
               />
-
               <View style={styles.linkContainer}>
                 <Text style={styles.linkText}>Ich habe die</Text>
-                <Link
-                  href='rulesModal/modal'
-                  style={[styles.link, themeStyles.link]}
-                >
+                <Link href='rulesModal/modal' style={[styles.link, themeStyles.link]}>
                   Richtlinien
                 </Link>
                 <Text style={styles.linkText}>gelesen und akzeptiert.</Text>
               </View>
             </View>
             <TextInput
-              style={[
-                styles.input,
-                styles.inputQuestion,
-                themeStyles.inverseTextInput,
-              ]}
+              style={[styles.input, styles.inputQuestion, themeStyles.inverseTextInput]}
               onChangeText={setQuestion}
               value={question}
               placeholder='Frage (Pflicht)'
@@ -352,7 +261,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-
   headerButton: {
     backgroundColor: "transparent",
     marginRight: 20,
@@ -376,33 +284,17 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     fontSize: 16,
   },
-  inputName: {},
-  inputAge: {},
-  inputEmail: {},
-  checkboxContainerGender: {
-    flexDirection: "row",
-    flexWrap: "wrap",
-    justifyContent: "space-around",
-    marginRight: 10,
-    marginTop: 30,
-    marginBottom: 30,
-  },
-  checkboxView: {
-    flexDirection: "row",
-    justifyContent: "center",
-    marginLeft: 20,
-  },
-  ContainerModal: {
-    marginTop: 10,
-    marginBottom: 10,
+  pickerTrigger: {
     marginHorizontal: 10,
     paddingHorizontal: 12,
+    marginTop: 10,
+    marginBottom: 10,
     paddingVertical: 10,
     borderWidth: 1,
     borderRadius: 20,
-    fontSize: 16,
+    justifyContent: "center",
   },
-  modalTextInput: {
+  pickerText: {
     textAlign: "center",
     fontSize: 16,
   },
@@ -417,13 +309,6 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     padding: 20,
   },
-  checkboxElementGender: {
-    borderRadius: 30,
-    marginRight: 7,
-  },
-
-  genderLable: {},
-  marjaLable: {},
   rules: {
     marginTop: 15,
     marginBottom: 15,
@@ -451,7 +336,6 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     fontSize: 14,
   },
-
   inputQuestion: {
     flex: 1,
     marginBottom: 50,
